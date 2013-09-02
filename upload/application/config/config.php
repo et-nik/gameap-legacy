ae9cd6f54aec45f570c8fa575ae0b89b79b409fd
ET-NiK <kronstadtsky@bk.ru>
2013-09-02 21:45:46 +0400

    Update 0.7.2

--- upload/application/config/config.php
+++ upload/application/config/config.php
@@ -44,7 +44,7 @@ $config['index_page'] = '';
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']	= 'AUTO';
+$config['uri_protocol']	= 'PATH_INFO';
 
 /*
 |--------------------------------------------------------------------------
