2441fd7a173a113beb8a82b64549c18514a7535c
ET-NiK <kronstadtsky@bk.ru>
2013-09-07 16:27:34 +0400

    Fix 0.8 dev1 errors

--- upload/application/config/autoload.php
+++ upload/application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('parser');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('form', 'url', 'language');
+$autoload['helper'] = array('form', 'url', 'language', 'modules');
 
 
 /*
