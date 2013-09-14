1821e65210b3c0c66a0994000ca254a4a34c6448
ET-NiK <kronstadtsky@bk.ru>
2013-09-14 14:24:01 +0400

    Support SAMP Query

--- upload/application/libraries/Query/drivers/Query_samp.php
+++ upload/application/libraries/Query/drivers/Query_samp.php
@@ -1,4 +1,4 @@
-<?php
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 /**
  *
  * @author Edward McKnight (EM-Creations.co.uk)
@@ -14,99 +14,16 @@
 // Credits to Westie for the original PHP SA-MP API and inspiration for this API.
 * *****************************************************************/
 
-class SampQuery {
+/*
+ * Modify by ET-NiK
+ * Special for GameAP (http://www.gameap.ru) 
+*/
+
+class Query_samp extends CI_Driver {
     private $sock = null;
-    private $server = null;
-    private $port = null;
+	var $host;
+	var $port;
 	
-    /**
-    *	Creates a new SampQuery object.
-    *	@param $server server hostname
-    *	@param $port port of the server
-    */
-    public function __construct($server, $port=7777)
-    {
-        $this->server = $server;
-        $this->port = $port;
-
-        $this->sock = fsockopen("udp://".$this->server, $this->port, $errorNum, $errorString, 2);
-        socket_set_timeout($this->sock, 2);
-    }
-
-    /**
-    * Returns an array of server information. 
-    * @return Array[]
-    * (
-    *   [password] => 0 or 1
-    *   [players] => players
-    *   [maxplayers] => maxPlayers
-    *   [hostname] => hostName
-    *   [gamemode] => gamemode
-    *   [map] => map
-    * )
-    */
-    public function getInfo() {
-        @fwrite($this->sock, $this->assemblePacket("i"));
-
-        fread($this->sock, 11);
-
-        $serverInfo = array();
-
-        $serverInfo['password'] = (integer) ord(fread($this->sock, 1));
-
-        $serverInfo['players'] = (integer) $this->toInt(fread($this->sock, 2));
-
-        $serverInfo['maxplayers'] = (integer) $this->toInt(fread($this->sock, 2));
-
-        $strLen = ord(fread($this->sock, 4));
-        if(!$strLen) return -1;
-
-        $serverInfo['hostname'] = (string) fread($this->sock, $strLen);
-
-        $strLen = ord(fread($this->sock, 4));
-        $serverInfo['gamemode'] = (string) fread($this->sock, $strLen);
-
-        $strLen = ord(fread($this->sock, 4));
-        $serverInfo['map'] = (string) fread($this->sock, $strLen);
-
-        return $serverInfo;
-    }
-
-
-    /**
-    * Returns a multidimensional array of basic player information.
-    * @return Array[]
-    * (
-    *   [0] => Array[]
-    *       (
-    *           [name] => playerName
-    *           [score] => score
-    *       )
-    *	...
-    * )
-    * @see getDetailedPlayers()
-    */
-    public function getBasicPlayers() {
-        @fwrite($this->sock, $this->assemblePacket("c"));
-        fread($this->sock, 11);
-
-        $playerCount = ord(fread($this->sock, 2));
-        $players = array();
-
-        if($playerCount > 0) {
-            for($i = 0; $i < $playerCount; ++$i) {
-                $strLen = ord(fread($this->sock, 1));
-                $players[$i] = array
-                (
-                    "name" => (string) fread($this->sock, $strLen),
-                    "score" => (integer) $this->toInt(fread($this->sock, 4)),
-                );
-            }
-        }
-        return $players;
-    }
-
-
     /**
     * Returns a multidimensional array of detailed player information. 
     * @return Array[]
@@ -144,52 +61,6 @@ class SampQuery {
         return $players;
     }
 
-
-    /**
-    * Returns an array of server rules.
-    * @return Array[]
-    * (
-    *   [gravity] => gravity
-    *   [mapname] => map
-    *   [version] => version
-    *   [weather] => weather
-    *   [weburl] => weburl
-    *   [worldtime] => worldtime
-    * )
-    */
-    public function getRules() {
-        @fwrite($this->sock, $this->assemblePacket("r"));
-        fread($this->sock, 11);
-
-        $ruleCount = ord(fread($this->sock, 2));
-        $rules = array();
-
-        for($i = 0; $i< $ruleCount; ++$i) {
-            $strLen = ord(fread($this->sock, 1));
-            $rule = (string) fread($this->sock, $strLen);
-
-            $strLen = ord(fread($this->sock, 1));
-            $rules[$rule] = (string) fread($this->sock, $strLen);
-        }
-        return $rules;
-    }
-    
-    /**
-    * Returns the server's ping.
-    * @return integer
-    */
-    public function getPing() {
-        $ping = 0;
-        $beforeSend = microtime(true);
-        @fwrite($this->sock, $this->assemblePacket("r"));
-        fread($this->sock, 15);
-        $afterReceive = microtime(true);
-        
-        $ping = ($afterReceive - $beforeSend) * 1000;
-        
-        return round($ping);
-    }
-
     private function toInt($string) {
         if($string === "") {
             return null;
@@ -218,7 +89,7 @@ class SampQuery {
 
     private function assemblePacket($type) {
         $packet = "SAMP";
-        $packet .= chr(strtok($this->server, "."));
+        $packet .= chr(strtok($this->host, "."));
         $packet .= chr(strtok("."));
         $packet .= chr(strtok("."));
         $packet .= chr(strtok("."));
@@ -233,8 +104,15 @@ class SampQuery {
     * Attempts to connect to the server and returns whether it was successful.
     * @return boolean
     */
-    public function connect() {
+    public function connect($host, $port) {
         $connected = false;
+        
+        $this->host = $host;
+        $this->port = $port;
+        
+        $this->sock = fsockopen("udp://" . $this->host, $this->port, $errorNum, $errorString, 2);
+        socket_set_timeout($this->sock, 2);
+        
         fwrite($this->sock, $this->assemblePacket("p0101"));
 
         if(fread($this->sock, 10)) {
@@ -245,10 +123,142 @@ class SampQuery {
         return $connected;
     }
     
-    /**
-    * Closes the connection
-    */
-    public function close() {
-        @fclose($this->sock);
-    }
+    
+    // ------------------------------------------------------------------------
+	
+	/**
+	 * Получение списка игроков на сервере
+	*/
+	function get_players($host, $port)
+	{
+		if (!$this->sock) {
+			$this->connect($host, $port);
+		}
+		
+		@fwrite($this->sock, $this->assemblePacket("c"));
+        fread($this->sock, 11);
+
+        $playerCount = ord(fread($this->sock, 2));
+        $players = array();
+
+        if($playerCount > 0) {
+            for($i = 0; $i < $playerCount; ++$i) {
+                $strLen = ord(fread($this->sock, 1));
+                $players[$i] = array
+                (
+                    "names" => (string) fread($this->sock, $strLen),
+                    "score" => (integer) $this->toInt(fread($this->sock, 4)),
+                    "connected" => '',
+                );
+            }
+        }
+		
+		return $players;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Получение информации о сервере
+	*/
+	function get_info($host, $port)
+	{
+		if (!$this->sock) {
+			$this->connect($host, $port);
+		}
+		
+		@fwrite($this->sock, $this->assemblePacket("i"));
+
+        fread($this->sock, 11);
+
+        $info = array();
+
+        $info['password'] = (integer) ord(fread($this->sock, 1));
+        $info['players'] = (integer) $this->toInt(fread($this->sock, 2));
+        $info['maxplayers'] = (integer) $this->toInt(fread($this->sock, 2));
+
+        $strLen = ord(fread($this->sock, 4));
+        if(!$strLen) return -1;
+
+        $info['hostname'] = (string) fread($this->sock, $strLen);
+
+        $strLen = ord(fread($this->sock, 4));
+        $info['gamemode'] = (string) fread($this->sock, $strLen);
+
+        $strLen = ord(fread($this->sock, 4));
+        $info['map'] = (string) fread($this->sock, $strLen);
+        
+
+		$info['game'] 			= 'San Andreas Multuplayer';
+		$info['game_code'] 		= 'samp';
+		$info['version'] 		= '';
+		$info['os'] 			= 'Unknown';
+
+		return $info;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Получение переменных сервера
+	*/
+	function get_rules($host, $port)
+	{
+		if (!$this->sock) {
+			$this->connect($host, $port);
+		}
+		
+		@fwrite($this->sock, $this->assemblePacket("r"));
+        fread($this->sock, 11);
+
+        $ruleCount = ord(fread($this->sock, 2));
+        $rules = array();
+
+        for($i = 0; $i< $ruleCount; ++$i) {
+            $strLen = ord(fread($this->sock, 1));
+            $rule = (string) fread($this->sock, $strLen);
+
+            $strLen = ord(fread($this->sock, 1));
+            $rules[$rule] = (string) fread($this->sock, $strLen);
+        }
+        return $rules;
+		
+		//~ $rules['rule'] = $request['Rule Name'];
+		//~ $rules['value'] = $request['Rule Value'];
+		
+		return $rules;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Статус сервера
+	*/
+	function get_status($host, $port)
+	{
+		return (bool)$this->ping($host, $port);
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Пинг сервера
+	*/
+	function ping($host, $port)
+	{
+		if (!$this->sock) {
+			$this->connect($host, $port);
+		}
+		
+		$ping = 0;
+        $beforeSend = microtime(true);
+        @fwrite($this->sock, $this->assemblePacket("r"));
+        fread($this->sock, 15);
+        $afterReceive = microtime(true);
+        
+        $ping = ($afterReceive - $beforeSend) * 1000;
+        
+        return round($ping);
+	}
+    
 }
