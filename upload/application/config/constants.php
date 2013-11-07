ada23ac41216f7df1010144135b54143776c3055
ET-NiK <kronstadtsky@bk.ru>
2013-11-08 00:54:56 +0400

    Update version information

--- upload/application/config/constants.php
+++ upload/application/config/constants.php
@@ -44,8 +44,8 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 |
 */
 
-define('AP_VERSION',	'0.8.2');
-define('AP_DATE',	'24.10.2013');
+define('AP_VERSION',	'0.8.4');
+define('AP_DATE',	'06.11.2013');
 
 
 /* End of file constants.php */
