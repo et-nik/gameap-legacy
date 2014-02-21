c62c8b0d4bee721cd10cca1113539fdc5fdca6f9
ET-NiK <kronstadtsky@bk.ru>
2014-02-21 16:45:45 +0400

    "Installer" драйвер для San-Andreas Multiplayer

---
+++ upload/application/libraries/Installer/drivers/Installer_samp.php
@@ -0,0 +1,95 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Game AdminPanel (АдминПанель)
+ *
+ * 
+ *
+ * @package		Game AdminPanel
+ * @author		Nikita Kuznetsov (ET-NiK)
+ * @copyright	Copyright (c) 2014, Nikita Kuznetsov (http://hldm.org)
+ * @license		http://www.gameap.ru/license.html
+ * @link		http://www.gameap.ru
+ * @filesource
+*/
+
+// ------------------------------------------------------------------------
+
+/**
+ * San-Andreas Multiplayer Installer драйвер
+ *
+ * Драйвер для установки игровых серверов Minecraft
+ *
+ * @package		Game AdminPanel
+ * @category	Drivers
+ * @author		Nikita Kuznetsov (ET-NiK)
+ * @sinse		0.9
+*/
+
+class Installer_samp extends CI_Driver {
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Список стандартных карт
+	*/
+	private function _get_default_map($game_code = '')
+	{
+		return true;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Получение параметра запуска игры
+	*/
+	public function get_start_command($game_code = '', $os = 'linux')
+	{
+		switch(strtolower($os)) {
+			case 'windows':
+				$start_command = "samp03svr.exe";
+				break;
+				
+			default:
+				$start_command = './samp03svr';
+				break;
+		}
+		
+		return $start_command;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Получение настроек для сервера по умолчанию
+	*/
+	public function get_default_parameters($game_code = 'samp', $os = 'linux', $parameters = array())
+	{
+		//~ $parameters['maxplayers'] 	= isset($this->_parameters['maxplayers']) ? $this->_parameters['maxplayers'] : 32;
+		
+		return array();
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * Правка конфигурационных файлов
+	*/
+	public function change_config()
+	{
+		$CI =& get_instance();
+		
+		$file = 'server.cfg';
+		$file_contents = read_ds_file($file, $this->server_data);
+		
+		// Костыль. Меняет права файла на 666
+		send_command('chmod 666 {dir}/server.cfg', $this->server_data);
+		
+		// Установка портов
+		$file_contents = change_value_on_file($file_contents, 'port', $this->server_data['server_port']);
+		$file_contents = change_value_on_file($file_contents, 'rcon_password', random_string('alnum', 8));
+		
+		$write_result = write_ds_file($file, $file_contents, $this->server_data);
+		
+		return true;
+	}
+}
