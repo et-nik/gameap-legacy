2159ada22691c9924f56f9d780410c19b6099785
Nikita <kronstadtsky@bk.ru>
2014-05-16 22:50:36 +0400

    Миграция. Обновление схемы бд.

---
+++ upload/application/migrations/008_version093.php
@@ -0,0 +1,27 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Version093 extends CI_Migration {
+	
+	public function up() {
+
+		$this->load->dbforge();
+		
+		if (!$this->db->field_exists('script_send_command', 'game_types')) {
+			$fields = array(
+							'script_send_command' => array(
+													 'type' => 'TINYTEXT',
+											  ),	
+			);
+
+			$this->dbforge->add_column('game_types', $fields, 'script_get_console');
+		}
+		
+		$this->db->update('game_types', array('script_send_command' => 'send_command {dir} {name} {ip} {port} "{command}" {user}'));
+
+	}
+	
+	public function down() {
+		$this->dbforge->drop_column('game_types', 'script_send_command');
+	}
+	
+}
