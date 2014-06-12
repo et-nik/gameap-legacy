e4120444aa0a89e51b6f966689d9838a2456ae5d
Nikita <kronstadtsky@bk.ru>
2014-06-12 14:18:36 +0400

    Драйверы капч.

--- upload/application/config/drivers.php
+++ upload/application/config/drivers.php
@@ -1,4 +1,6 @@
 <?php
 
-$config['drivers']['rcon'] = array('rcon_goldsource', 'rcon_source', 'rcon_minecraft', 'rcon_samp', 'rcon_rust');
+$config['drivers']['rcon'] 		= array('rcon_goldsource', 'rcon_source', 'rcon_minecraft', 'rcon_samp', 'rcon_rust');
 //~ $config['drivers']['query'] = array('query_goldsource', 'query_source', 'query_minecraft', 'query_samp');
+
+$config['drivers']['captcha'] 	= array('captcha_codeigniter', 'captcha_recaptcha');
