3bf77eb010c5fb29da9968a03bd754343f9e407a
ET-NiK <kronstadtsky@bk.ru>
2014-10-24 19:15:59 +0400

    Обновлены сведения о версии.

--- upload/application/config/constants.php
+++ upload/application/config/constants.php
@@ -81,7 +81,7 @@ define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 */
 
 define('AP_VERSION',	'1.0-dev');
-define('AP_DATE',	'05.10.2014');
+define('AP_DATE',	'24.10.2014');
 
 
 /* End of file constants.php */
