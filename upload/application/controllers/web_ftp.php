cd651257f30929a8d94dea33c401b421d4abe160
ET-NiK <kronstadtsky@bk.ru>
2015-01-16 21:42:42 +0300

    Web FTP

---
+++ upload/application/controllers/web_ftp.php
@@ -0,0 +1,202 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Game AdminPanel (АдминПанель)
+ *
+ * 
+ *
+ * @package		Game AdminPanel
+ * @author		Nikita Kuznetsov (ET-NiK)
+ * @copyright	Copyright (c) 2013-2015, Nikita Kuznetsov (http://hldm.org)
+ * @license		http://gameap.ru/license.html
+ * @link		http://gameap.ru
+ * @filesource
+*/
+class Web_ftp extends CI_Controller { 
+	
+	//Template
+	var $tpl_data = array();
+	
+	var $user_data = array();
+	var $server_data = array();
+	
+	// -----------------------------------------------------------------
+	
+	public function __construct()
+    {
+        parent::__construct();
+		
+		$this->load->database();
+        $this->load->model('users');
+        $this->lang->load('server_files');
+        $this->lang->load('server_command');
+        $this->lang->load('server_control');
+
+        if ($this->users->check_user()) {
+			//Base Template
+			$this->tpl_data['title'] 		= lang('server_files_title_index');
+			$this->tpl_data['heading']		= lang('server_files_header_index');
+			$this->tpl_data['content'] 		= '';
+			$this->tpl_data['menu'] 		= $this->parser->parse('menu.html', $this->tpl_data, true);
+			$this->tpl_data['profile'] 		= $this->parser->parse('profile.html', $this->users->tpl_userdata(), true);
+        
+        } else {
+            redirect('auth');
+        }
+    }
+    
+    // -----------------------------------------------------------------
+    
+    // Отображение информационного сообщения
+    function _show_message($message = false, $link = false, $link_text = false)
+    {
+        
+        if (!$message) {
+			$message = lang('error');
+		}
+		
+        if (!$link) {
+			$link = 'javascript:history.back()';
+		}
+		
+		if (!$link_text) {
+			$link_text = lang('back');
+		}
+
+        $local_tpl['message'] = $message;
+        $local_tpl['link'] = $link;
+        $local_tpl['back_link_txt'] = $link_text;
+        $this->tpl_data['content'] = $this->parser->parse('info.html', $local_tpl, true);
+        $this->parser->parse('main.html', $this->tpl_data);
+    }
+    
+    // -----------------------------------------------------------------
+	
+	/**
+	 * Получение данных фильтра для вставки в шаблон
+	 */
+	private function _get_tpl_filter($filter = false)
+	{
+		$this->load->model('servers');
+		
+		if (!$filter) {
+			$filter = $this->users->get_filter('servers_list');
+		}
+		
+		$this->servers->select_fields('game, server_ip');
+		
+		$games_array 	= array();
+		$ip_array		= array();
+
+		if ($servers_list = $this->servers->get_list()) {
+			foreach($this->servers->get_list() as $server) {
+				if (!in_array($server['game'], $games_array)) {
+					$games_array[] 	= $server['game'];
+				}
+				
+				if (!in_array($server['server_ip'], $ip_array)) {
+					$ip_array[ $server['server_ip'] ]		= $server['server_ip'];
+				}
+			}
+		}
+		
+		if (empty($this->games->games_list)) {
+			$this->games->get_active_games_list();
+		}
+		
+		foreach($this->games->games_list as &$game) {
+			$games_option[ $game['code'] ] = $game['name'];
+		}
+		
+		$tpl_data['filter_name']			= isset($filter['name']) ? $filter['name'] : '';
+		$tpl_data['filter_ip']				= isset($filter['ip']) ? $filter['ip'] : '';
+		
+		$tpl_data['filter_ip_dropdown']		= form_multiselect('filter_ip[]', $ip_array, $tpl_data['filter_ip']);
+		
+		$default = isset($filter['game']) ? $filter['game'] : null;
+		$tpl_data['filter_games_dropdown'] 	= form_multiselect('filter_game[]', $games_option, $default);
+		
+		return $tpl_data;
+	}
+
+	// ----------------------------------------------------------------
+
+    /**
+     * Главная страница
+     * 
+    */
+    public function index()
+    {
+		/* Загружаем модель */
+		$this->load->model('servers');
+		$this->load->model('servers/games');
+		$this->load->helper('games');
+		
+		$filter = $this->users->get_filter('servers_list');
+		$local_tpl = $this->_get_tpl_filter($filter);
+		
+		$this->servers->set_filter($filter);
+		$this->servers->get_servers_list($this->users->auth_id);
+		
+		$local_tpl['url'] 			= site_url('web_ftp/server');
+		$local_tpl['games_list'] = servers_list_to_games_list($this->servers->servers_list);
+			
+		$this->tpl_data['content'] .= $this->parser->parse('servers/select_server.html', $local_tpl, true);
+			
+		$this->parser->parse('main.html', $this->tpl_data);
+	}
+	
+	// ----------------------------------------------------------------
+
+    /**
+     * Главная страница
+     * 
+    */
+    public function server($server_id = false)
+    {
+		$this->load->helper('ds');
+		$this->load->driver('files');
+		
+		/* 
+		 * Если не указан id сервера, то перенаправляем на
+		 * страницу выбора 
+		*/
+		if(!$server_id){
+			redirect('admin/servers_files');
+		}
+		
+		/* Преобразование id в числовое значение */
+		$server_id = (int)$server_id;
+		
+		/* Загружаем модель работы с сервером */
+		$this->load->model('servers');
+		
+		/* Проверка привилегий на сервер */
+		$this->users->get_server_privileges($server_id);
+		
+		/* Проверка на права загрузки и правки конфигурационный файлов сервера */
+		if(!$this->users->auth_servers_privileges['UPLOAD_CONTENTS']
+		&& !$this->users->auth_servers_privileges['CHANGE_CONFIG']
+		){
+			$this->_show_message(lang('server_files_no_privileges'), site_url('admin/servers_files'));
+			return false;
+		}
+		
+		$local_tpl = array();
+		
+		/* Получение данных сервера */
+		$this->servers->get_server_data($server_id);
+		
+		/* Получение данных сервера */
+		if (!$this->servers->get_server_data($server_id)) {
+			$this->_show_message(lang('server_control_server_not_found'));
+			return;
+		}
+		
+		/* Получение данных сервера для шаблона */
+		$local_tpl['server_id'] = $this->servers->server_data['id'];
+
+		$this->tpl_data['content'] .= $this->parser->parse('web_ftp/web_ftp.html', $local_tpl, true);
+		$this->parser->parse('main.html', $this->tpl_data);
+	}
+
+}
---
+++ upload/themes/system/js/web_ftp.js
@@ -0,0 +1,238 @@
+var current_dir = "/";
+var current_file = "";
+var file_contents = "";
+
+var last = true;
+	
+var csrf_token_name = page.csrf_token_name;
+
+function GetList() { 
+	
+	ShowLoad();
+	
+	$('#files > tbody').each(function() {
+		$(this).append($('.mydiv'));
+		$(this).children().not('#head').not('#up').remove();
+	});
+	
+	ajax_data = {'dir':current_dir};
+	ajax_data[csrf_token_name] = page.csrf_hash;
+	
+	$.ajax({ 
+		url:     sprintf("%sajax/web_ftp/get_list/%s", page.site_url, page.server_id),
+		type:     "POST",
+		data: 	ajax_data,
+		dataType: "json",
+		success: function(response) {
+
+			if (response.status == '0') {
+				noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
+				return;
+			}
+			
+			$.each(response.files, function(key, val) {
+				
+				icon = response.files[key].type == 'd' 
+					? sprintf('<img src="%s/images/folder_16.png" />', page.template_files)
+					: sprintf('<img src="%s/images/file_16.png" />', page.template_files);
+				
+				$('#files').append('<tr class="list">\
+				<td>' + icon + '</td>\
+				<td onclick="tr_action(\'' + response.files[key].file_name + '\', \'' + response.files[key].type + '\')">' + response.files[key].file_name + '</td>\
+				<td>' + response.files[key].file_size + '</td>\
+				<td>\
+					<a href="#" title="Rename" onclick="RenameFilePopup(\''+response.files[key].file_name+'\');return false;"><img src="'+page.template_files+'/images/rename_16.png" /></a>\
+					<a href="#" title="Delete" onclick="DeleteFilePopup(\''+response.files[key].file_name+'\');return false;"><img src="'+page.template_files+'/images/delete_16.png" /></a>\
+				</td>\
+				</tr>');
+
+			});
+			
+			$("#current_dir").val(current_dir);
+			last = true;
+		}, 
+		error: function() {
+			noty({layout: 'bottomCenter', type: 'error', text: "unknown error"});
+			last = false;
+		},
+		complete: function() {
+			HideLoad();
+		}
+	}); 
+}
+
+function ReadFile(file) {
+	if (current_file == file & file_contents != '') {
+		$("textarea[name='file_contents']").val(file_contents);
+		$('#edit_file').arcticmodal();
+		$('#edit_file h3').html(page.lang_server_files_edit + ' ' + file);
+		return;
+	}
+	
+	ajax_data = {'dir':current_dir, 'file':file};
+	ajax_data[csrf_token_name] = page.csrf_hash;
+
+	$.ajax({ 
+		url:     sprintf("%sajax/web_ftp/read_file/%s", page.site_url, page.server_id),
+		type:     "POST",
+		data: ajax_data,
+		dataType: "json",
+		success: function(response) {
+			
+			if (response.status == '0') {
+				noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
+				return;
+			}
+			
+			current_file = file;
+			file_contents = response.file_contents;
+			
+			$("textarea[name='file_contents']").val(response.file_contents);
+			$('#edit_file').arcticmodal();
+			$('#edit_file h3').html(page.lang_server_files_edit + ' ' + file);
+		}, 
+		error: function() {
+			noty({layout: 'bottomCenter', type: 'error', text: "unknown error"});
+		} 
+	}); 
+}
+
+function DeleteFilePopup(file) {
+	$("input[name='new_file_name']").val(file);
+	$('#delete_confirm').arcticmodal();
+	current_file = file;
+}
+
+function DeleteFile() {
+	ajax_data = {'dir':current_dir, 'file':current_file};
+	ajax_data[csrf_token_name] = page.csrf_hash;
+
+	$.ajax({ 
+		url:     sprintf("%sajax/web_ftp/delete_file/%s", page.site_url, page.server_id),
+		type:     "POST",
+		data: ajax_data,
+		dataType: "json",
+		success: function(response) {
+			
+			if (response.status == '0') {
+				noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
+				return;
+			}
+		}, 
+		error: function() {
+			noty({layout: 'bottomCenter', type: 'error', text: "unknown error"});
+		} 
+	}); 
+	
+	GetList();
+	$('#delete_confirm').arcticmodal('close');
+}
+
+function RenameFilePopup(file) {
+	$("input[name='new_file_name']").val(file);
+	$('#rename').arcticmodal();
+	current_file = file;
+}
+
+function RenameFile() {
+	ajax_data = {'dir':current_dir, 'file':current_file, 'new_name':$("input[name='new_file_name']").val()};
+	ajax_data[csrf_token_name] = page.csrf_hash;
+	
+	$.ajax({ 
+		url:     sprintf("%sajax/web_ftp/rename_file/%s", page.site_url, page.server_id),
+		type:     "POST",
+		data: ajax_data,
+		dataType: "json",
+		success: function(response) {
+			
+			if (response.status == '0') {
+				noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
+				return;
+			}
+		}, 
+		error: function() {
+			noty({layout: 'bottomCenter', type: 'error', text: "unknown error"});
+		} 
+	}); 
+	
+	GetList();
+	$('#rename').arcticmodal('close');
+}
+
+function WriteFile() {
+	if (current_file == '') {
+		noty({layout: 'bottomCenter', type: 'error', text: "empty file"});
+		return;
+	}
+
+	ajax_data = {'dir':current_dir, 'file':current_file, 'file_contents':$("textarea[name='file_contents']").val()};
+	ajax_data[csrf_token_name] = page.csrf_hash;
+	
+	$.ajax({ 
+		url:     sprintf("%sajax/web_ftp/write_file/%s", page.site_url, page.server_id),
+		type:     "POST",
+		data: ajax_data,
+		dataType: "json",
+		success: function(response) {
+			
+			if (response.status == '0') {
+				noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
+				return;
+			}
+			
+			noty({layout: 'bottomCenter', type: 'success', text: page.lang_server_files_data_writed});
+			
+			file_contents = $("textarea[name='file_contents']").val();
+		}, 
+		error: function() {
+			noty({layout: 'bottomCenter', type: 'error', text: "unknown error"});
+		} 
+	});
+}
+
+function UploadFile()
+{
+	$("#fupload_file" ).append('<input type="hidden" name="dir" value="'+current_dir+'" />');
+			
+	var formData = new FormData($("#fupload_file")[0]);
+	$.ajax({
+		url:     sprintf("%sajax/web_ftp/upload/%s", page.site_url, page.server_id),
+		type: 'POST',
+		data: formData,
+		cache: false,
+		contentType: false,
+		processData: false,
+		xhr: function() {
+			var myXhr = $.ajaxSettings.xhr();
+			if(myXhr.upload){
+				myXhr.upload.addEventListener('progress',progressHandlingFunction, false);
+			}
+			return myXhr;
+		},
+		dataType: "json",
+		success: function(response) {
+			if (response.status == '0') {
+				noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
+				return;
+			}
+			
+			noty({layout: 'bottomCenter', type: 'success', text: "File uploaded"});
+		}, 
+		error: function() {
+			noty({layout: 'bottomCenter', type: 'error', text: "unknown error"});
+		} 
+	});
+}
+
+function tr_action(name, type)
+{
+	if (type == 'f') {
+		ReadFile(name);
+	}
+	else if (type == 'd') {
+		current_dir = current_dir + '/' + name;
+		current_dir = current_dir.replace(/\/+/, '/');
+		
+		GetList();
+	}
+} 
