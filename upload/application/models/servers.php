9ce62c0ab67aa80af62d171109ec9b06718aec6e
ET-NiK <kronstadtsky@bk.ru>
2015-03-06 15:46:22 +0300

    Проверка на случай, если json_decode вернет false

--- upload/application/models/servers.php
+++ upload/application/models/servers.php
@@ -639,6 +639,10 @@ class Servers extends CI_Model {
 		/* Данные алиасов в массив */
 		$this->server_data['aliases_values'] = json_decode($this->server_data['aliases'], true);
 		
+		if (!is_array($this->server_data['aliases_values'])) {
+			$this->server_data['aliases_values'] = array();
+		}
+
 		/* Получаем query и rcon порты */
 		if (!$this->server_data['query_port']) {
 			$this->server_data['query_port'] = $this->server_data['server_port'];
