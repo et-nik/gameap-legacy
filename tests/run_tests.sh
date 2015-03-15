1d538f1946b2d2f8751af986454cf8413a2db455
ET-NiK <kronstadtsky@bk.ru>
2015-03-16 01:57:06 +0300

    Install SSH and Telnet

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,14 @@
 cd tests/GDaemon && ./daemon
 cd ../../
+
+sudo apt-get update -qq
+sudo apt-get install -y -qq libssh2-1-dev libssh2-php
+pecl install -f ssh2-beta < .noninteractive
+sudo apt-get install -y -qq telnetd
+whoami
+echo `whoami`":1234" | sudo chpasswd
+
 sudo netstat -lntup
+
+
 phpunit --configuration tests/phpunit.xml 
