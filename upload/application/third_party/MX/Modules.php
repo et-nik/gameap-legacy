8f28efefbd314d379fe1ff93833326f1b45cab05
ET-NiK <kronstadtsky@bk.ru>
2015-03-15 13:51:16 +0300

    Обновлены файлы фреймворка

--- upload/application/third_party/MX/Modules.php
+++ upload/application/third_party/MX/Modules.php
@@ -5,7 +5,7 @@
 global $CFG;
 
 /* get module locations from config settings or use the default module location and offset */
-is_array(Modules::$locations = $CFG->item('modules_locations')) OR Modules::$locations = array(
+is_array(Modules::$locations = config_item('modules_locations')) OR Modules::$locations = array(
 	APPPATH.'modules/' => '../modules/',
 );
 
