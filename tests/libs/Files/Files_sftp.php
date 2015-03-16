6fdeec3aaf10a2a1d32dddf6b297a60586e392fc
ET-NiK <kronstadtsky@bk.ru>
2015-03-16 03:05:57 +0300

    List files

--- tests/libs/Files/Files_sftp.php
+++ tests/libs/Files/Files_sftp.php
@@ -21,7 +21,7 @@ class Files_sftp_test extends CIUnit_TestCase
 
 		$this->assertTrue($this->CI->files->connect($config));
 		
-		//~ print_r($this->CI->files->list_files('.'));
+		var_dump($this->CI->files->list_files('/home/travis/build/ET-NiK/GameAP/Files/'));
     }
 
     public function test_upload()
