61bb8dfbe3759e572f82e15273088836d2c5f0c5
ET-NiK <kronstadtsky@bk.ru>
2015-03-30 16:13:57 +0300

    CIUnit define ENVIRONMENT

--- upload/application/third_party/CIUnit/bootstrap_phpunit.php
+++ upload/application/third_party/CIUnit/bootstrap_phpunit.php
@@ -6,6 +6,25 @@ if ( ! defined('CIUnit_Version') ) {
 
 /*
  *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ */
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+/*
+ *---------------------------------------------------------------
  * PHP ERROR REPORTING LEVEL
  *---------------------------------------------------------------
  *
