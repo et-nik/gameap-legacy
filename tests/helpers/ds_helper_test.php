e414c0736f46702e7978c0f1e9f703abe14f34e7
ET-NiK <kronstadtsky@bk.ru>
2015-06-22 20:50:33 +0300

    Обновлены тесты для ds_helper

--- tests/helpers/ds_helper_test.php
+++ tests/helpers/ds_helper_test.php
@@ -13,11 +13,43 @@ class Ds_helper_test extends CIUnit_TestCase
     public function test_replace_shotcodes()
     {
 		$server_data = array(
-			'server_ip' => '127.0.0.1',
-			'screen_name' => 'gameap',
+			'server_ip' 	=> '127.0.0.1',
+			'server_port' 	=> '27015',
+			'screen_name' 	=> 'gameap',
+			'query_port' 	=> '27016',
+			'rcon_port' 	=> '27017',
+			
+			'start_command' => 'start',
+			'id' 			=> 1337,
+			'dir' 			=> 'my_server',
+			'script_path' 	=> '/home/servers',
+			'screen_name' 	=> 'screen',
+			'start_code' 	=> 'cstrike',
+			'su_user' 		=> 'nik',
+			
+			'cpu_limit' 		=> '50',
+			'ram_limit' 		=> '256',
+			'net_limit' 		=> '1000',
+			
+			'aliases_list'	=> json_encode(array(array('alias' => 'maxplayers'))),
+			'aliases_values'	=> array('maxplayers' => '32'),
 		);
 		
-		$command = '{ip} {name}';
-        $this->assertEquals('127.0.0.1 gameap', replace_shotcodes($command, $server_data));
+		$this->assertEquals('start', replace_shotcodes('{command}', $server_data));
+		$this->assertEquals('1337', replace_shotcodes('{id}', $server_data));
+		$this->assertEquals('script_path', replace_shotcodes('{script_path}', $server_data));
+		$this->assertEquals('my_server', replace_shotcodes('{game_dir}', $server_data));
+		$this->assertEquals('/home/servers/my_server', replace_shotcodes('{dir}', $server_data));
+		$this->assertEquals('screen', replace_shotcodes('{name}', $server_data));
+		$this->assertEquals('127.0.0.1', replace_shotcodes('{ip}', $server_data));
+		$this->assertEquals('27015', replace_shotcodes('{port}', $server_data));
+		$this->assertEquals('27016', replace_shotcodes('{query_port}', $server_data));
+		$this->assertEquals('27017', replace_shotcodes('{rcon_port}', $server_data));
+		$this->assertEquals('nik', replace_shotcodes('{user}', $server_data));
+		
+		$this->assertEquals('32', replace_shotcodes('{maxplayers}', $server_data));
+		
+		$command = '{dir} {ip} {name} {query_port} {rcon_port} {maxplayers}';
+        $this->assertEquals('/home/servers/my_server 127.0.0.1 gameap 27016 27017 32', replace_shotcodes($command, $server_data));
     }
 }
