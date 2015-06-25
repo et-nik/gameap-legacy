5b2d050e714035dd8a636932b94868a9f1e4e44e
ET-NiK <kronstadtsky@bk.ru>
2015-06-25 12:52:36 +0300

    Fixed test

--- tests/helpers/date_helper_test.php
+++ tests/helpers/date_helper_test.php
@@ -4,31 +4,31 @@ class Date_helper_test extends CIUnit_TestCase
 {
 	public function setUp()
     {
-        date_default_timezone_set('Europe/Moscow');
+        date_default_timezone_set("Greenwich");
         $this->CI->load->helper('date');
     }
     
     public function test_add_month_to_unix_time()
     {
 		$this->assertEquals(1437816012, add_month_to_unix_time(1435224012)); // On month
-		$this->assertEquals(1498296012, add_month_to_unix_time(1435224012, 2)); // Two month
+		$this->assertEquals(1440408012, add_month_to_unix_time(1435224012, 2)); // Two month
 	}
 	
 	public function test_add_year_to_unix_time()
 	{
 		$this->assertEquals(1466760093, add_year_to_unix_time(1435224093)); 
-		$this->assertEquals(1466846554, add_year_to_unix_time(1498468954)); // leap-year
+		$this->assertEquals(1530004954, add_year_to_unix_time(1498468954)); // leap-year
 	}
 	
 	public function test_unix_to_human()
 	{
-		$this->assertEquals('25-06-2015 12:20', unix_to_human(1435224012)); 
-		$this->assertEquals('25-06-2015 12:20:12', unix_to_human(1435224012, true)); 
+		$this->assertEquals('25-06-2015 09:20', unix_to_human(1435224012)); 
+		$this->assertEquals('25-06-2015 09:20:12', unix_to_human(1435224012, true)); 
 	}
 	
 	public function test_human_to_unix()
 	{
-		$this->assertEquals('1435224012', human_to_unix('25-06-2015 12:20:12')); 
-		$this->assertEquals('1435224000', human_to_unix('25-06-2015 12:20')); 
+		$this->assertEquals('1435224012', human_to_unix('25-06-2015 09:20:12')); 
+		$this->assertEquals('1435224000', human_to_unix('25-06-2015 09:20')); 
 	}
 } 
