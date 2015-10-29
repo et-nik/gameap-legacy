611277ec2495ebd69aeb000bde076e2f609b206a
ET-NiK <kronstadtsky@bk.ru>
2015-10-29 15:30:08 +0300

    Исправлена ошибка в тестах (Попытка №3)

--- tests/helpers/ds_helper_test.php
+++ tests/helpers/ds_helper_test.php
@@ -83,7 +83,7 @@ class Ds_helper_test extends CIUnit_TestCase
 			'dir' 		=> 'cstrike',
 		)));
 		
-		$this->assertEquals('/home/serv01/cstrike', $this->_get_ds_file_path(array(
+		$this->assertEquals('/home/serv01/cstrike/', $this->_get_ds_file_path(array(
 			'ftp_host' 	=> 'localhost',
 			'ftp_path' 	=> '/home/serv01//',
 			'dir' 		=> '/cstrike/',
@@ -95,7 +95,7 @@ class Ds_helper_test extends CIUnit_TestCase
 			'dir' 		=> 'cstrike',
 		)));
 		
-		$this->assertEquals('/home/serv01/cstrike', $this->_get_ds_file_path(array(
+		$this->assertEquals('/home/serv01/cstrike/', $this->_get_ds_file_path(array(
 			'ssh_host' 	=> 'localhost',
 			'ssh_path' 	=> '/home/serv01//',
 			'dir' 		=> '/cstrike/',
@@ -107,7 +107,7 @@ class Ds_helper_test extends CIUnit_TestCase
 			'dir' 		=> 'cstrike',
 		)));
 		
-		$this->assertEquals('/home/serv01/cstrike', $this->_get_ds_file_path(array(
+		$this->assertEquals('/home/serv01/cstrike/', $this->_get_ds_file_path(array(
 			'gdaemon_host' 	=> 'localhost',
 			'script_path' 	=> '/home/serv01//',
 			'dir' 		=> '/cstrike/',
@@ -119,7 +119,7 @@ class Ds_helper_test extends CIUnit_TestCase
 			'dir' 		=> 'cstrike',
 		)));
 		
-		$this->assertEquals('/home/serv01/cstrike', $this->_get_ds_file_path(array(
+		$this->assertEquals('/home/serv01/cstrike/', $this->_get_ds_file_path(array(
 			'local_server' => 1,
 			'script_path' 	=> '/home/serv01//',
 			'dir' 		=> '/cstrike/',
