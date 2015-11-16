6fbbad69141af47f1ca5ea44354ff5b90da9e0e9
ET-NiK <kronstadtsky@bk.ru>
2015-11-16 20:14:18 +0300

    Хуки

--- upload/application/config/hooks.php
+++ upload/application/config/hooks.php
@@ -31,5 +31,19 @@ $hook['post_controller'][] = array(
   'filepath' => 'hooks'
 );
 
+$hook['post_controller'][] = array(
+  'class'    => 'Tpl_replace',
+  'function' => 'parse_notices',
+  'filename' => 'tpl_replace.php',
+  'filepath' => 'hooks'
+);
+
+$hook['display_override'] = array(
+    'class' => 'DisplayHook',
+    'function' => 'captureOutput',
+    'filename' => 'DisplayHook.php',
+    'filepath' => 'hooks'
+  );
+  
 /* End of file hooks.php */
 /* Location: ./application/config/hooks.php */
