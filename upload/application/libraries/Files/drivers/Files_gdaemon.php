4140ea0371689f9a74ae1e8bd85b7433e45cf941
ET-Nik <nikita.hldm@gmail.com>
2016-06-03 22:24:09 +0300

    Update GDaemon files driver

--- upload/application/libraries/Files/drivers/Files_gdaemon.php
+++ upload/application/libraries/Files/drivers/Files_gdaemon.php
@@ -40,12 +40,25 @@ class Files_gdaemon extends CI_Driver {
 	private $_auth		= false;
 	
 	private $_max_file_size = 1000000;
+    private $_max_bufsize = 10240;
+
+    private $_timeout = 10;
 
     private $_CI;
 
     private $_write_binn;
     private $_read_binn;
-	
+
+    const FSERV_AUTH        = 1;
+    const FSERV_FILESEND    = 3;
+    const FSERV_READDIR     = 4;
+    const FSERV_MKDIR       = 5;
+    const FSERV_MOVE        = 6;
+    const FSERV_REMOVE      = 7;
+
+    const FSERV_UPLOAD_TO_SERVER        = 1;
+    const FSERV_DOWNLOAD_FR_SERVER     = 2;
+        
 	// -----------------------------------------------------------------
 	
 	function __construct()
@@ -131,7 +144,7 @@ class Files_gdaemon extends CI_Driver {
 	{
         $this->_binn_free();
         
-        $this->_write_binn->add_int16(1);
+        $this->_write_binn->add_int16(self::FSERV_AUTH);
         $this->_write_binn->add_str($this->username);
         $this->_write_binn->add_str($this->password);
         $this->_write_binn->add_int16(3); // Set mode DAEMON_SERVER_MODE_FILES
@@ -159,7 +172,7 @@ class Files_gdaemon extends CI_Driver {
 	 */
 	private function _read()
 	{
-        return substr(socket_read($this->_socket, 10240), 0, -4);
+        return substr(socket_read($this->_socket, $this->_max_bufsize), 0, -4);
 	}
 	
 	// -----------------------------------------------------------------
@@ -180,9 +193,9 @@ class Files_gdaemon extends CI_Driver {
 	
 	function close()
 	{
-		if (!$this->_socket && is_resource($this->_socket)) {
-			return;
-		}
+		if ($this->_connection) {
+            socket_close($this->_socket);
+        }
 	}
 	
 	// -----------------------------------------------------------------
@@ -196,14 +209,19 @@ class Files_gdaemon extends CI_Driver {
 		if (!$this->hostname OR !$this->port) {
 			$this->_error('server_command_empty_connect_data');
 		}
-
-        $this->_socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        socket_connect($this->_socket, $this->hostname, $this->port);
-		
-		if (!$this->_socket) {
+        
+        $this->_connection = @fsockopen($this->hostname, $this->port, $errno, $errstr, 10);
+        
+        if (!$this->_connection) {
 			$this->_error('server_command_connection_failed');
 		}
-		
+        
+        $this->_socket   = @socket_import_stream($this->_connection);
+
+        stream_set_timeout($this->_connection, $this->_timeout);
+        socket_set_option($this->_socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => $this->_timeout, 'usec' => 0));
+        socket_set_option($this->_socket, SOL_SOCKET, SO_SNDTIMEO, array('sec'=> $this->_timeout, 'usec' => 0));
+        
 		$this->_auth = false;
 		$this->_login();
 		
@@ -228,9 +246,64 @@ class Files_gdaemon extends CI_Driver {
 	 * @param string	привилегии
 	 * @return bool
 	 */
-	public function upload($locpath, $rempath, $mode = 'auto', $permissions = NULL)
+	public function upload($locpath, $rempath, $mode = 'auto', $permissions = null)
 	{
-		
+		if (!$this->_connection OR !$this->_auth) {
+			$this->_error('server_command_not_connected');
+		}
+
+        if (!file_exists($locpath)) {
+            $this->_error('web_ftp_file_not_found');
+        }
+
+        if (filesize($locpath) > $this->_max_file_size) {
+			$this->_error('web_ftp_file_big');
+		}
+
+        if ($permissions == null) {
+            $permissions = 666;
+        }
+
+        $this->_binn_free();
+
+        $this->_write_binn->add_int16(self::FSERV_FILESEND);
+        $this->_write_binn->add_uint8(self::FSERV_UPLOAD_TO_SERVER);
+        $this->_write_binn->add_str($rempath);
+        $this->_write_binn->add_uint64(filesize($locpath));
+        $this->_write_binn->add_uint16($permissions);
+        $this->_write_binn->add_bool(true); // Make dirs
+
+        socket_write($this->_socket, $this->_write_binn->get_binn_val() . "\xFF\xFF\xFF\xFF");
+
+        $read = $this->_read();
+
+        if (!$read) {
+            $this->_error('Read socket error');
+        }
+
+        $this->_read_binn->binn_open($read);
+        $results = $this->_read_binn->get_binn_arr();
+
+        if ($results[0] != 100) {
+            // Error
+            $this->_error("server_command_gdaemon_sendfile_error", $results[1]);
+        }
+
+        $handle = fopen($locpath, "r");
+
+        while(!feof($handle)) {
+            socket_write($this->_socket, fread($handle, $this->_max_bufsize));
+        }
+        
+        $this->_read_binn->binn_open($read);
+        $results = $this->_read_binn->get_binn_arr();
+
+        if ($results[0] != 100) {
+            $this->_error("server_command_gdaemon_sendfile_error", $results[1]);
+        }
+        else {
+            return true;
+        }
 	}
 	
 	// -----------------------------------------------------------------
@@ -259,7 +332,7 @@ class Files_gdaemon extends CI_Driver {
 	*/
 	public function delete_dir($filepath)
 	{
-		// return $this->delete_file($filepath);
+		return $this->delete_file($filepath);
 	}
 	
 	// -----------------------------------------------------------------
@@ -272,7 +345,37 @@ class Files_gdaemon extends CI_Driver {
 	 */
 	public function delete_file($filepath)
 	{
-		
+		if (!$filepath) {
+			$this->_error('server_files_directory_no_set');
+		}
+
+		if (!$this->_connection OR !$this->_auth) {
+			$this->_error('server_command_not_connected');
+		}
+
+        $this->_binn_free();
+
+        $this->_write_binn->add_int16(self::FSERV_REMOVE);
+        $this->_write_binn->add_str($filepath);
+        $this->_write_binn->add_bool(true);                // Recursively
+
+        socket_write($this->_socket, $this->_write_binn->get_binn_val() . "\xFF\xFF\xFF\xFF");
+        $read = $this->_read();
+
+        if (!$read) {
+            $this->_error('Read socket error');
+        }
+
+        $this->_read_binn->binn_open($read);
+        $results = $this->_read_binn->get_binn_arr();
+
+        if ($results[0] != 100) {
+            // Error
+            $this->_error("server_command_gdaemon_remove_error", $results[1]);
+            return false;
+        }
+
+        return true;
 	}
 	
 	// -----------------------------------------------------------------
@@ -286,7 +389,59 @@ class Files_gdaemon extends CI_Driver {
 	 */
 	public function download($rempath, $locpath)
 	{
-		
+		if (!$this->_connection OR !$this->_auth) {
+			$this->_error('server_command_not_connected');
+		}
+
+        if (!$locpath) {
+            $this->_error('gdaemon_locpath_not_set');
+        }
+
+        $this->_binn_free();
+
+        $this->_write_binn->add_int16(self::FSERV_FILESEND);
+        $this->_write_binn->add_uint8(self::FSERV_DOWNLOAD_FR_SERVER);
+        $this->_write_binn->add_str($rempath);
+
+        socket_write($this->_socket, $this->_write_binn->get_binn_val() . "\xFF\xFF\xFF\xFF");
+        $read = $this->_read();
+
+        if (!$read) {
+            $this->_error('Read socket error');
+        }
+
+        $this->_read_binn->binn_open($read);
+        $results = $this->_read_binn->get_binn_arr();
+
+        if ($results[0] != 100) {
+            // Error
+            $this->_error("gdaemon_download_file_error", $results[1]);
+            return false;
+        }
+
+        $filesize = $results[2];
+        $writed = 0;
+        $readlen = 0;
+
+        $output_file = fopen($locpath, "w");
+
+        if (!$output_file) {
+            $this->_error("fopen_error");
+        }
+
+        while($writed < $filesize) {
+            if ($filesize - $writed > $this->_max_bufsize) {
+                $readlen = $this->_max_bufsize;
+            }
+            else {
+                $readlen = $filesize - $writed;
+            }
+
+            $writed += fwrite($output_file, socket_read($this->_socket, $readlen));
+        }
+
+        fclose($output_file);
+        return true;
 	}
 	
 	// -----------------------------------------------------------------
@@ -331,13 +486,13 @@ class Files_gdaemon extends CI_Driver {
 			$this->_error('server_files_directory_no_set');
 		}
 
-		if (!$this->_socket OR !$this->_auth) {
+		if (!$this->_connection OR !$this->_auth) {
 			$this->_error('server_command_not_connected');
 		}
 
         $this->_binn_free();
 
-        $this->_write_binn->add_int16(4);       // Read dir
+        $this->_write_binn->add_uint16(self::FSERV_READDIR);
         $this->_write_binn->add_str($path);     // Dir path
         $this->_write_binn->add_uint8(1);       // Mode
 
@@ -388,13 +543,13 @@ class Files_gdaemon extends CI_Driver {
 			$this->_error('server_files_directory_no_set');
 		}
 
-		if (!$this->_socket OR !$this->_auth) {
+		if (!$this->_connection OR !$this->_auth) {
 			$this->_error('server_command_not_connected');
 		}
 
         $this->_binn_free();
 
-        $this->_write_binn->add_int16(4);       // Read dir
+        $this->_write_binn->add_int16(self::FSERV_READDIR);
         $this->_write_binn->add_str($path);     // Dir path
         $this->_write_binn->add_uint8(1);       // Mode
 
@@ -457,7 +612,37 @@ class Files_gdaemon extends CI_Driver {
 	 */
 	public function mkdir($path = '', $permissions = 0755)
 	{
-		
+		if (!$path) {
+			$this->_error('server_files_directory_no_set');
+		}
+
+		if (!$this->_connection OR !$this->_auth) {
+			$this->_error('server_command_not_connected');
+		}
+
+        $this->_binn_free();
+
+        $this->_write_binn->add_int16(self::FSERV_MKDIR);
+        $this->_write_binn->add_str($path);
+        $this->_write_binn->add_str($permissions);
+
+        socket_write($this->_socket, $this->_write_binn->get_binn_val() . "\xFF\xFF\xFF\xFF");
+        $read = $this->_read();
+
+        if (!$read) {
+            $this->_error('Read socket error');
+        }
+
+        $this->_read_binn->binn_open($read);
+        $results = $this->_read_binn->get_binn_arr();
+
+        if ($results[0] != 100) {
+            // Error
+            $this->_error("server_command_gdaemon_mkdir_error", $results[1]);
+            return false;
+        }
+
+        return true;
 	}
 	
 	// -----------------------------------------------------------------
@@ -467,7 +652,38 @@ class Files_gdaemon extends CI_Driver {
 	 */
 	public function rename($old_file, $new_file)
 	{
-		
+		if (!$old_file OR !$new_file) {
+			$this->_error('server_command_gdaemon_empty_parameters');
+		}
+
+		if (!$this->_connection OR !$this->_auth) {
+			$this->_error('server_command_not_connected');
+		}
+
+        $this->_binn_free();
+
+        $this->_write_binn->add_int16(self::FSERV_MOVE);
+        $this->_write_binn->add_str($old_file);
+        $this->_write_binn->add_str($new_file);
+        $this->_write_binn->add_bool(false);            // Copy
+
+        socket_write($this->_socket, $this->_write_binn->get_binn_val() . "\xFF\xFF\xFF\xFF");
+        $read = $this->_read();
+
+        if (!$read) {
+            $this->_error('Read socket error');
+        }
+
+        $this->_read_binn->binn_open($read);
+        $results = $this->_read_binn->get_binn_arr();
+
+        if ($results[0] != 100) {
+            // Error
+            $this->_error("server_command_gdaemon_rename_error", $results[1]);
+            return false;
+        }
+
+        return true;
 	}
 	
 	// -----------------------------------------------------------------
@@ -477,7 +693,7 @@ class Files_gdaemon extends CI_Driver {
 	 */
 	public function move($old_file, $new_file)
 	{
-
+        $this->rename($old_file, $new_file);
 	}
 	
 	// -----------------------------------------------------------------
