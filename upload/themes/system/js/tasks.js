a96035cdfb8c4102f63eb466425393a8c5437a1a
ET-Nik <nikita.hldm@gmail.com>
2016-06-15 19:10:09 +0300

    Update code in tasks.js

--- upload/themes/system/js/tasks.js
+++ upload/themes/system/js/tasks.js
@@ -27,137 +27,92 @@ function get_task_info(task_id)
     });
 }
 
-check_step = 0;
-task_result = 0;
 function check_task(task, task_id)
 {
-    intervalID = null;
+    get_task_info(task_id);
 
-    function check_task_start()
-    {
-        get_task_info(task_id);
-
-        if (task_status != 'waiting' && task_status != '') {
-            clearInterval(intervalID);
-            check_step = 1;
-            check_task(task, task_id);
-        }
+    if (task_status == '') {
+        $("#vts-content").html("...");
+        set_progress(0);
+    } else if (task_status == 'waiting') {
+        $("#vts-content").html(messages[task][0] + "...");
+        set_progress(5);
     }
-
-    function check_task_end()
-    {
-        get_task_info(task_id);
-
-        if ((task_status == 'success' || task_status == 'error') && task_status != '') {
-            clearInterval(intervalID);
-            check_step = 2;
-            check_task(task, task_id);
-        }
+    else if (task_status == 'working') {
+        $("#vts-content").html(messages[task][1] + "...");
+        set_progress(40);
     }
-    
-    function check_task_result()
-    {
+    else if (task_status == 'success') {
+        $("#vts-content").html(messages[task][2] + "...");
+
         if (task != 'gsstart' && task != 'gsstop' && task != 'gsrest') {
-            clearInterval(intervalID);
-
-            task_result = 1;
-            check_step = 3;
-            check_task(task, task_id);
+            set_progress(100);
+            report_task_end(task, true);
+            return;
         }
+        
+        set_progress(80);
 
         if (server_status == 0) {
             get_server_status(page.server_id);
         }
-        
-        if (task == 'gsstart' || task == 'gsrest') {
-            if (server_status == 2) {
-                clearInterval(intervalID);
 
-                task_result = 1;
-                check_step = 3;
-                check_task(task, task_id);
-            } else if (server_status == 3) {
-                clearInterval(intervalID);
-
-                task_result = 0;
-                check_step = 3;
-                check_task(task, task_id);
+        if (server_status == 2) {
+            // Server online
+            if (task == 'gsstart') {
+                set_progress(100);
+                report_task_end(task, true);
+                return;
+            } else if (task == 'gsstop') {
+                set_progress(100);
+                report_task_end(task, false);
+                return;
+            }
+        } else if (server_status == 3) {
+            // Server offline
+            if (task == 'gsstart') {
+                set_progress(100);
+                report_task_end(task, false);
+                return;
+            } else if (task == 'gsstop') {
+                set_progress(100);
+                report_task_end(task, true);
+                return;
             }
         }
-
-        if (task == 'gsstop') {
-            if (server_status == 3) {
-                clearInterval(intervalID);
-
-                task_result = 1;
-                check_step = 3;
-                check_task(task, task_id);
-            } else if (server_status == 2) {
-                clearInterval(intervalID);
-
-                task_result = 0;
-                check_step = 3;
-                check_task(task, task_id);
-            }
-        }
-        
-        // check_task(task, task_id);
+    }
+    else if (task_status == 'error') {
+        set_progress(100);
+        report_task_end(task, false);
+        return;
     }
 
-    if (check_step == 0) {
-        $(".task-progress-header").html(message_title[task]);
-        $("#vts-content").html("");
-        $("#vts-content").append("<span class=\"vts-progress\">" + messages[task][0] + "...</span>");
-        intervalID = setInterval(check_task_start, check_timeout);
-    }
+    setTimeout(check_task, check_timeout, task, task_id);
+}
 
-    if (!intervalID && check_step == 1) {
-        $('.vts-progress').last().remove();
-        $("#vts-content").append("<span class=\"vts-progress\">&#10003; " + messages[task][0] + "</span><br />\n");
-        $("#vts-content").append("<span class=\"vts-progress\">" + messages[task][1] + "...</span>");
-        
-        intervalID = setInterval(check_task_end, check_timeout);
-    }
-
-    if (!intervalID && check_step == 2) {
-        $('.vts-progress').last().remove();
-        $("#vts-content").append("<span class=\"vts-progress\">&#10003; " + messages[task][1] + "</span><br />\n");
-        $("#vts-content").append("<span class=\"vts-progress\">" + messages[task][2] + "...</span>\n");
-        intervalID = setInterval(check_task_result, check_timeout);
-    }
-
-    if (!intervalID && check_step == 3) {
-        // End
-        $('.vts-progress').last().remove();
-        $("#vts-content").append("<span class=\"vts-progress\">&#10003; " + messages[task][2] + "</span><br />\n");
-
-        if (task_result) {
-            $('#view-task-status').arcticmodal('close');
-            noty({
-                layout: 'center',
-                type: 'success',
-                text: success_messages[task],
-                callback: {
-                    onClose: function() {
-                        location.reload();
-                    }
+function report_task_end(task, success = false)
+{
+    if (success) {
+        tmodal_close();
+        noty({
+            layout: 'center',
+            type: 'success',
+            text: success_messages[task],
+            callback: {
+                onClose: function() {
+                    location.reload();
                 }
-            });
-        }
-        else {
-            $('#view-task-status').arcticmodal('close');
-            
-            noty({
-                layout: 'center',
-                type: 'error',
-                text: error_messages[task]
-            });
-        }
-
-        // Var nulled
-        check_step      = 0;
-        task_status     = '';
-        task_result     = 0;
+            }
+        });
+    }
+    else {
+        tmodal_close();
+        
+        noty({
+            layout: 'center',
+            type: 'error',
+            text: error_messages[task]
+        });
     }
 }
 
@@ -169,24 +124,22 @@ function add_task(task)
 
     block = true;
     ShowLoad();
+
+    ajax_data = {'task': task, 'server_id': page.server_id};
+	ajax_data[page.csrf_token_name] = page.csrf_hash;
     
     $.ajax({
         url: sprintf("%sajax/tasks/add_task", page.site_url),
         type: "POST",
-        data:
-        {
-            'task': task,
-            'server_id': {server_id},
-            '{csrf_token_name}': '{csrf_hash}'
-        },
+        data: ajax_data,
         dataType: "json",
         success: function(response) {
             if (response.status == '0') {
                 noty({layout: 'bottomCenter', type: 'error', text: response.error_text});
                 return;
             } else {
-                // noty({layout: 'bottomCenter', type: 'success', text: "Success"});
-                $('#view-task-status').arcticmodal();
+                tmodal_open();
+                $('.task-progress-header').html(message_titles[task]);
                 check_task(task, response.task_id);
             }
         },
@@ -197,25 +150,5 @@ function add_task(task)
             HideLoad();
             block = false;
         }
-    });
-}
-
-function server_act(task)
-{
-    noty({
-        layout: 'center',
-        type: 'confirm',
-        text: confirm_msg[task],
-        buttons: [
-            {addClass: 'btn btn-primary', text: '{lang_yes}', onClick: function($noty) {
-                    $noty.close();
-                    add_task(task);
-                }
-            },
-            {addClass: 'btn btn-danger', text: '{lang_no}', onClick: function($noty) {
-                    $noty.close();
-                }
-            }
-        ]
     });
 }
