7c0406afefe0b1b96c5fe4049dcf6360ffdc4359
ET-Nik <nikita.hldm@gmail.com>
2016-10-04 19:00:29 +0300

    Uncomment encrypt key parameter from config

--- upload/application/config/config.php
+++ upload/application/config/config.php
@@ -245,7 +245,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-//~ $config['encryption_key'] = '';
+$config['encryption_key'] = 'encrypt_super_secret_key198';
 
 /*
 |--------------------------------------------------------------------------
