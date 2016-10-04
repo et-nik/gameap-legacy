5961889c80c96aecbccc5283395a7d1e25cb16a7
ET-Nik <nikita.hldm@gmail.com>
2016-10-04 11:47:27 +0300

    Setup migrations in tests

--- tests/Test.php
+++ tests/Test.php
@@ -6,9 +6,29 @@ class Test extends CI_Controller
     {
         parent::__construct();
 
-        include_once FCPATH . '../../../install_gameap/db.php';
+        $this->load->database();
+        include FCPATH . '../../../install_gameap/db.php';
+
+        if (!$this->db->table_exists('migrations')) {
+			$this->dbforge->add_field(array(
+				'version' => array('type' => 'INT', 'constraint' => 3),
+			));
+
+			$this->dbforge->create_table('migrations', TRUE);
+		}
+		
+		include APPPATH . 'config/migration.php';
+		
+		if (isset($config['migration_version'])) {
+			$m_version = $config['migration_version'];
+		} else {
+			$m_version = 0;
+		}
+		
+		$this->db->insert('migrations', array('version' => $m_version));
 
         $this->load->library('migration');
+        
         if (!$this->migration->latest()) {
 			show_error($this->migration->error_string());
 		}
