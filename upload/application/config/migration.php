867a59663ec31650c256f989171f9c6ae252e4fe
ET-Nik <nikita.hldm@gmail.com>
2016-12-08 00:03:37 +0300

    CI 2.1.3 -> CI 3.1.0. Updated migration config

--- upload/application/config/migration.php
+++ upload/application/config/migration.php
@@ -10,37 +10,6 @@
 */
 $config['migration_enabled'] = TRUE;
 
-/*
-|--------------------------------------------------------------------------
-| Migration Type
-|--------------------------------------------------------------------------
-|
-| Migration file names may be based on a sequential identifier or on
-| a timestamp. Options are:
-|
-|   'sequential' = Sequential migration naming (001_add_blog.php)
-|   'timestamp'  = Timestamp migration naming (20121031104401_add_blog.php)
-|                  Use timestamp format YYYYMMDDHHIISS.
-|
-| Note: If this configuration value is missing the Migration library
-|       defaults to 'sequential' for backward compatibility with CI2.
-|
-*/
-$config['migration_type'] = 'sequential';
-
-/*
-|--------------------------------------------------------------------------
-| Migrations table
-|--------------------------------------------------------------------------
-|
-| This is the name of the table that will store the current migrations state.
-| When migrations runs it will store in a database table which migration
-| level the system is at. It then compares the migration level in this
-| table to the $config['migration_version'] if they are not the same it
-| will migrate up. This must be set.
-|
-*/
-$config['migration_table'] = 'migrations';
 
 /*
 |--------------------------------------------------------------------------
