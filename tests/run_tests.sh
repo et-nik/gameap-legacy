2888686fa4109486c27ad93770fe53dd55caaccd
ET-Nik <nikita.hldm@gmail.com>
2016-12-12 22:54:54 +0300

    Updated run_test.sh script. ROOT_DIR var

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,20 +5,22 @@ echo `whoami`":1234" | sudo chpasswd
 
 sudo netstat -lntup
 
-# Make files
-sudo mkdir /home/travis/build/ET-NiK/GameAP/Files
-sudo chmod 777 /home/travis/build/ET-NiK/GameAP/Files
+ROOT_DIR=/home/travis/build/ET-NiK/GameAP
 
-echo FILE_CONTENTS >> /home/travis/build/ET-NiK/GameAP/Files/File01.txt
-echo FILE_CONTENTS >> /home/travis/build/ET-NiK/GameAP/Files/File02.txt
-sudo chmod 666 /home/travis/build/ET-NiK/GameAP/Files/File01.txt
-sudo chmod 666 /home/travis/build/ET-NiK/GameAP/Files/File02.txt
+# Make files
+sudo mkdir ${ROOT_DIR}/Files
+sudo chmod 777 ${ROOT_DIR}/Files
+
+echo FILE_CONTENTS >> ${ROOT_DIR}/Files/File01.txt
+echo FILE_CONTENTS >> ${ROOT_DIR}/Files/File02.txt
+sudo chmod 666 ${ROOT_DIR}/Files/File01.txt
+sudo chmod 666 ${ROOT_DIR}/Files/File02.txt
 
 phpunit --version
 
-mv /home/travis/build/ET-NiK/GameAP/tests/Test.php /home/travis/build/ET-NiK/GameAP/upload/application/controllers/Test.php
-mv /home/travis/build/ET-NiK/GameAP/tests/gameap_config.php /home/travis/build/ET-NiK/GameAP/upload/application/config/gameap_config.php
-phpunit --configuration /home/travis/build/ET-NiK/GameAP/tests/phpunit-dbinst.xml
+mv ${ROOT_DIR}/tests/Test.php ${ROOT_DIR}/upload/application/controllers/Test.php
+mv ${ROOT_DIR}/tests/gameap_config.php ${ROOT_DIR}/upload/application/config/gameap_config.php
+phpunit --configuration ${ROOT_DIR}/tests/phpunit-dbinst.xml
 
-phpunit --configuration /home/travis/build/ET-NiK/GameAP/tests/phpunit.xml
+phpunit --configuration ${ROOT_DIR}/tests/phpunit.xml
 exit $?
