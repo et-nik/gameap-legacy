7d3bd029137f31b04e8779c3ec0fcd5b20121781
ET-Nik <nikita.hldm@gmail.com>
2017-01-14 15:25:48 +0300

    Deleted log file

--- upload/application/logs/log-2016-12-22.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-12-22 18:19:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) /var/www/gameap/upload/system/database/drivers/mysqli/mysqli_driver.php 202
-ERROR - 2016-12-22 18:19:45 --> Unable to connect to the database
-ERROR - 2016-12-22 18:20:08 --> Severity: Notice --> Undefined index: register_users /var/www/gameap/upload/application/views/default/login.html 41
