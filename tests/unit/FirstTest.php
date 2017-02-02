38cc599eee43ddeb5691765d7fd02341c670212d
ET-Nik <nikita.hldm@gmail.com>
2017-02-02 16:19:05 +0300

    FirstTest

---
+++ tests/unit/FirstTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class FirstTest extends \Codeception\TestCase\Test {
+
+    protected function _before()
+    {
+
+    }
+
+    protected function _after()
+    {
+
+    }
+
+    public function testSomethingHere()
+    {
+        $this->assertTrue(true, true);
+    }
+}
