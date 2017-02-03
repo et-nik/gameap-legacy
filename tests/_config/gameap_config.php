5c9271f78546fe3091bfc8d3097f5c0042d54b66
ET-Nik <nikita.hldm@gmail.com>
2017-02-03 04:03:54 +0300

    English language in tests

--- tests/_config/gameap_config.php
+++ tests/_config/gameap_config.php
@@ -1,3 +1,4 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['encryption_key'] = 'encrypt_super_secret_key';
+$config['encryption_key']   = 'encrypt_super_secret_key';
+$config['language']	        = 'english';
