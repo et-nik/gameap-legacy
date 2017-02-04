70a44014dfea66708d5f76495cb06cf4329d432a
ET-Nik <nikita.hldm@gmail.com>
2017-02-04 07:03:10 +0300

    Allow register users for tests.

--- tests/_config/gameap_config.php
+++ tests/_config/gameap_config.php
@@ -1,6 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['base_url']         = 'http://127.0.0.1:8888/';
+$config['register_users'] 	= true;
 $config['encryption_key']   = 'encrypt_super_secret_key';
 $config['language']	        = 'english';
 $config['captcha_driver']	= 'none';
