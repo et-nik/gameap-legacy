a78f8fe3b9776b00e2b6220bde2bf1c05025839e
ET-Nik <nikita.hldm@gmail.com>
2017-03-03 23:48:41 +0300

    Main page

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,12 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Game AdminPanel (АдминПанель)
- *
- * 
+ * Game AdminPanel (GameAP)
  *
  * @package		Game AdminPanel
  * @author		Nikita Kuznetsov (ET-NiK)
- * @copyright	Copyright (c) 2014, Nikita Kuznetsov (http://hldm.org)
+ * @copyright	Copyright (c) 2014-2017, Nikita Kuznetsov (http://hldm.org)
  * @license		http://www.gameap.ru/license.html
  * @link		http://www.gameap.ru
  * @filesource
@@ -15,9 +13,7 @@
 use \Myth\Controllers\BaseController;
 
 /**
- * Главная страница
- *
- * Отображение поля входа, либо перенаправление, если пользователь авторизован
+ * Main page
  *
  * @package		Game AdminPanel
  * @category	Controllers
@@ -29,10 +25,8 @@ class Main extends BaseController {
 
     public $tpl = array();
 
-    //Отображение
 	public function index()
 	{
-        /* Загрузка модели проверки пользователей */
         $this->load->database();
         $this->load->model('users');
         
@@ -40,32 +34,10 @@ class Main extends BaseController {
 
         $this->tpl['code'] = '';
 
-        /* 
-         * Проверяем пользователя 
-         * если все ок, то перенаправляем в панель
-        */
-        if(!$this->users->check_user()){
+        if (!$this->users->check_user()){
 			redirect('auth/in');
-		}
-		
-		/* Пользователь не авторизован, показываем ему форму авторизации */
-		// $this->tpl['menu'] 		= '';
-        // $this->tpl['profile'] 		= '';
-        // $this->tpl['content'] 		= '';
-        // $this->tpl['title'] 		= lang('auth_title_index');
-		// $this->tpl['heading'] 		= lang('auth_heading');
-
-        $local_tpl = array();
-
-        //Base Template
-        $this->tpl['title'] 	= lang('tasks_title_index');
-        $this->tpl['heading'] 	= lang('tasks_heading_index');
-        $this->tpl['content'] 	= '';
-        
-        $this->tpl['menu']      = $this->parser->parse('menu.html', $this->tpl, true);
-        $this->tpl['profile']   = $this->parser->parse('profile.html', $this->users->tpl_userdata(), true);
-
-        $this->tpl['content'] .= $this->parser->parse('main_page.html', $local_tpl, true);
-        $this->parser->parse('main.html', $this->tpl);
+		} else {
+            redirect('admin');
+        }
 	}
 }
