ed253925bb40584e05b6762c5694ec31f44d6d79
ET-Nik <nikita.hldm@gmail.com>
2017-09-10 21:14:39 +0300

    Fix syntax error

--- application/database/migrations/20170114224301_Install.php
+++ application/database/migrations/20170114224301_Install.php
@@ -622,7 +622,7 @@ class Migration_install extends CI_Migration {
             ],
             'ram' => [
                 'type' => 'TINYTEXT',
-                'default' => '',http://seasonvar.ru/actor/%D0%94%D0%B6%D0%B5%D1%81%D1%81%D0%B8%D0%BA%D0%B0%20%D0%91%D1%80%D0%B0%D1%83%D0%BD-%D0%A4%D0%B8%D0%BD%D0%B4%D0%BB%D0%B8
+                'default' => '',
             ],
             'cpu' => [
                 'type' => 'TINYTEXT',
