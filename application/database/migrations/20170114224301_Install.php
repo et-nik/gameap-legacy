0c161c999b9df775c0e2814913a1ee5e7c1f2b58
ET-Nik <nikita.hldm@gmail.com>
2017-09-10 19:04:18 +0300

    Add `server_stats` table

--- application/database/migrations/20170114224301_Install.php
+++ application/database/migrations/20170114224301_Install.php
@@ -608,6 +608,37 @@ class Migration_install extends CI_Migration {
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('servers');
 
+        // server_stats
+        $this->dbforge->add_field([
+            'id' => [
+                'type' => 'INT',
+                'auto_increment' => true
+            ],
+            'server_id' => [
+                'type' => 'INT'
+            ],
+            'time' => [
+                'type' => 'INT'
+            ],
+            'ram' => [
+                'type' => 'TINYTEXT',
+                'default' => '',http://seasonvar.ru/actor/%D0%94%D0%B6%D0%B5%D1%81%D1%81%D0%B8%D0%BA%D0%B0%20%D0%91%D1%80%D0%B0%D1%83%D0%BD-%D0%A4%D0%B8%D0%BD%D0%B4%D0%BB%D0%B8
+            ],
+            'cpu' => [
+                'type' => 'TINYTEXT',
+                'default' => '',
+            ],
+            'netstat' => [
+                'type' => 'TINYTEXT',
+                'default' => '',
+            ],
+            'drvspace' => [
+                'type' => 'TINYTEXT'
+            ],
+        ]);
+        $this->dbforge->add_key('id', true);
+        $this->dbforge->create_table('servers_stats');
+
         // servers_privileges
         $this->dbforge->add_field([
             'user_id' => [
