e0561c2d17ff5b69a70a06bdd893e6a045d6f4d7
et-nik <nikita.hldm@gmail.com>
2019-06-18 22:29:49 +0300

    Update server.sh

--- bin/Linux/server.sh
+++ bin/Linux/server.sh
@@ -29,239 +29,257 @@
 #
 
 # Версия
-VERSION=100
-
-# Загрузка конфигурации
-if [[ -s ./server.conf ]]; then
-	# echo -e "Configuration loaded"
-	source ./server.conf
-fi
+VERSION=101
 
 # Переменные
-USER=$(whoami);
+UUSER=$(whoami);
 NAME="";
 
 # ----------------------------------------------------------------------
 # Запуск сервера
-function server_start()
+server_start()
 {
-	if [[ $SNAME == "" ]]; then
-		echo -e "Screen name empty";
-		echo -e "Server not started";
-		return;
-	fi
-	
-	if [[ $COMMAND == "" ]]; then
-		echo -e "Command empty";
-		echo -e "Server not started";
-		return;
-	fi
-	
-	if [[ "$(server_status)" == 1 ]]
-       then
-		echo -e "Server is already running";
-    else
-		su $USER -c "cd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
-		PID=$!;
-		sleep 3;
-		
-		if [[ `su $USER -c "screen -U -ls | grep -i $SNAME"` ]]
-			then
-			echo -e "Server started";
-			# echo -e "Start command:\n cd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
-		else
-		   echo -e "Server not started";
-		   echo -e "Start command:\ncd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
-		fi
+    if [[ $SNAME == "" ]]; then
+        echo -e "Screen name empty";
+        echo -e "Server not started";
+        return 1;
     fi
     
-    cpu_limit;
+    if [[ $COMMAND == "" ]]; then
+        echo -e "Command empty";
+        echo -e "Server not started";
+        return 1;
+    fi
+    
+    if server_status; then
+        echo -e "Server is already running";
+        return 1
+    else
+        su $UUSER -c "cd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
+        PID=$!;
+        sleep 3;
+        
+        if su $UUSER -c "screen -U -ls | grep -i ${SNAME}"; then
+            echo -e "Server started";
+            return 0
+        else
+           echo -e "Server not started";
+           echo -e "Start command:\ncd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
+           return 1
+        fi
+    fi
 }
 
 # ----------------------------------------------------------------------
 # Остановка сервера
-function server_stop()
+server_stop()
 {
-	if [[ $SNAME == "" ]]; then
-		echo -e "Screen name empty";
-		echo -e "Server not started";
-		exit;
-	fi
-	
-	if [[ "$(server_status)" == 1 ]]
-       then
+    if [[ $SNAME == "" ]]; then
+        echo -e "Screen name empty";
+        echo -e "Server not started";
+        return 1
+    fi
+    
+    if server_status; then
        kill -TERM `ps aux | grep -v grep | grep -i screen | grep -i $SNAME | awk '{print $2}'`
-       su $USER -c  "screen -U -X -S $SNAME kill"
+       su $UUSER -c "screen -U -X -S $SNAME kill"
        echo "Server stopped"
+       return 0
     else
        echo "Coulnd't find a running server"
+       return 1
     fi
 }
 
 # ----------------------------------------------------------------------
 # Получение статуса сервера
-function server_status()
+server_status()
 {
-	if [[ $SNAME == '' ]]
-	then
-		echo 0;
-		return;
-	fi
+    if [[ $SNAME == '' ]]
+    then
+        return 1
+    fi
 
-	if [[ `sudo su $USER -c "screen -ls |grep $SNAME"` ]]
-		then
-		echo 1;
+    if su $UUSER -c "screen -ls | grep ${SNAME}"; then
+        return 0
     else
-		echo 0;
+        return 1
     fi
 }
 
 # ----------------------------------------------------------------------
 # Получение частей команд
-function get_parts()
+get_parts()
 {
-	if [[ $RAM_LIMIT > 0 && $allow_ram_limit ]]; then
-		COMMAND_PARTS[0]="ulimit -Hv $RAM_LIMIT ;";
-	fi
-	
-	if [[ $NET_LIMIT > 0 && $allow_net_limit ]]; then
-		COMMAND_PARTS[1]="trickle -d $NET_LIMIT -u $NET_LIMIT";
-	fi
+    if [[ $RAM_LIMIT > 0 && $allow_ram_limit ]]; then
+        COMMAND_PARTS[0]="ulimit -Hv $RAM_LIMIT ;";
+    fi
+    
+    if [[ $NET_LIMIT > 0 && $allow_net_limit ]]; then
+        COMMAND_PARTS[1]="trickle -d $NET_LIMIT -u $NET_LIMIT";
+    fi
 }
 
 # ----------------------------------------------------------------------
 # Применить ограничение CPU %
-function cpu_limit()
+cpu_limit()
 {
-	if [[ $CPU_LIMIT && $allow_cpu_limit && $PID ]]; then
-		CPU_LIMIT=$(($CPU_LIMIT*$core_count))
-		
-		# PID
-		cpulimit --pid=$PID --limit=$CPU_LIMIT
-		
-		# EXE
-		#cpulimit --exe="$DIR/$PROGRAM" --limit=$CPU_LIMIT
-	fi
+    if [[ $CPU_LIMIT && $allow_cpu_limit && $PID ]]; then
+        CPU_LIMIT=$(($CPU_LIMIT*$core_count))
+        
+        # PID
+        cpulimit --pid=$PID --limit=$CPU_LIMIT
+        
+        # EXE
+        #cpulimit --exe="$DIR/$PROGRAM" --limit=$CPU_LIMIT
+    fi
 }
 
-# Получение опций
-while getopts 't:d:n:i:p:c:umfs' opt ;
-do
-	case $opt in
-		t)
-			TYPE=$OPTARG;
-			;;
-		d) 
-			DIR=$OPTARG;
-			;;
-		n) 
-			SNAME=$OPTARG;
-			;;
-		i) 
-			IP=$OPTARG;
-			;;
-		p) 
-			PORT=$OPTARG;
-			;;
-		c) 
-			COMMAND=$OPTARG;
-			;;
-		u)
-			USER=$OPTARG;
-			;;
-		m)
-			RAM_LIMIT=$OPTARG;
-			;;
-		f)
-			CPU_LIMIT=$OPTARG;
-			;;
-		s)
-			NET_LIMIT=$OPTARG;
-			;;
-		esac
-done
+parse_options()
+{
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+        key="$1"
+        
+        case $key in
+            -t|--type)
+                TYPE="$2"
+                shift
+                shift
+            ;;
+            -d|--dir)
+                DIR="$2"
+                shift
+                shift
+            ;;
+            -n|--name|--sname)
+                SNAME="$2"
+                shift
+                shift
+            ;;
+            -i|--ip)
+                IP="$2"
+                shift
+                shift
+            ;;
+            -p|--port)
+                PORT="$2"
+                shift
+                shift
+            ;;
+            -c|--command)
+                COMMAND="$2"
+                shift
+                shift
+            ;;
+            -u|--user)
+                COMMAND="$2"
+                shift
+                shift
+            ;;
+            -m|--memory)
+                RAM_LIMIT="$2"
+                shift
+                shift
+            ;;
+            -f|--cpu)
+                CPU_LIMIT="$2"
+                shift
+                shift
+            ;;
+            -s|--net)
+                NET_LIMIT="$2"
+                shift
+                shift
+            ;;
+            *)
+                POSITIONAL+=("$1")
+                shift
+        esac
+    done
+}
 
-# DEBUG
-# -------------------
-#~ echo -e "Type: $TYPE";
-#~ echo -e "Dir: $DIR";
-#~ echo -e "Screen name: $SNAME";
-#~ echo -e "Ip: $IP";
-#~ echo -e "Port: $PORT";
-#~ echo -e "Command: $OPTARG";
-#~ echo -e "User: $USER";
-#~ echo -e "Memory limit: $RAM_LIMIT Kb";
-#~ echo -e "Cpu limit: $CPU_LIMIT %";
-#~ echo -e "Net limit: $NET_LIMIT Kb/s";
-# -------------------
+main()
+{
+    # Разбиение на программу и агрументы
+    IFS=' ' read -a explode <<< "$COMMAND";
+    PROGRAM=${explode[0]};
 
-# Разбиение на программу и агрументы
-IFS=' ' read -a explode <<< "$COMMAND";
-PROGRAM=${explode[0]};
-unset explode[0];
+    ARGUMENTS="";
+    for element in "${explode[@]}"
+    do
+        ARGUMENTS=$ARGUMENTS" $element";
+    done
+    
+    unset explode;
+    
+    case "$TYPE" in
+        start)
+            get_parts;
+            
+            if server_start; then
+                exit 0
+            else
+               exit 1
+            fi
+            
+            ;;
+    
+        stop)
+            if server_stop; then
+                exit 0
+            else
+               exit 1
+            fi
+            ;;
+            
+        restart)
+            get_parts >> /dev/null
+            server_stop >> /dev/null
+            
+            if server_start; then
+                echo -e "Server restarted"
+                exit 0
+            else
+                echo -e "Server not restarted"
+                exit 1
+            fi
+            
+            ;;
+            
+        status)
+            if server_status; then
+               echo "Server is UP"
+               exit 0
+            else
+               echo "Server is Down"
+               exit 1
+            fi
+            ;;
+            
+        get_console)
+            su $UUSER -c "screen -U -S $SNAME -X -p 0 hardcopy -h $DIR/gap_console.txt && chmod 666 $DIR/gap_console.txt"
+            sed -i '/^$/d' $DIR/gap_console.txt
+            cat $DIR/gap_console.txt
+            ;;
+            
+        send_command)
+            # Screen version 4.00.03jw4 (FAU) 2-May-06
+            #~ su $USER "-c screen -p 0 -S $SNAME -X stuff '$COMMAND'$'\n'"
+        
+            # Screen version 4.01.00devel (GNU) 2-May-06
+            #~ su $USER "-c screen -p 0 -S $SNAME -X stuff '$COMMAND\n'"
+    
+            su $UUSER "-c screen -U -p 0 -S $SNAME -X stuff '$COMMAND
+            '"
+            
+            ;;
+        *)
+            echo "Unknown type"
+            ;;
+    esac
+}
 
-ARGUMENTS="";
-for element in "${explode[@]}"
-do
-    ARGUMENTS=$ARGUMENTS" $element";
-done
-
-unset explode;
-
-case "$TYPE" in
-	start)
-		get_parts;
-		server_start;
-		;;
-
-	stop)
-		server_stop;
-		;;
-		
-	restart)
-		get_parts >> /dev/null
-		server_stop >> /dev/null
-		
-		if [ "$(server_start)" == "Server started" ] ;
-			then
-			echo -e "Server restarted"
-			exit 0
-		else
-			echo -e "Server not restarted"
-			exit 1
-		fi
-		
-		;;
-		
-	status)
-		if [ "$(server_status)" == 1 ] ;
-			then
-		   echo "Server is UP"
-		   exit 0
-		else
-		   echo "Server is Down"
-		   exit 1
-		fi
-		;;
-		
-	get_console)
-		su $USER -c "screen -U -S $SNAME -X -p 0 hardcopy -h $DIR/gap_console.txt && chmod 666 $DIR/gap_console.txt"
-		RESULT=`cat $DIR/gap_console.txt`
-		echo -e "$RESULT"
-		;;
-		
-	send_command)
-		# Screen version 4.00.03jw4 (FAU) 2-May-06
-		#~ su $USER "-c screen -p 0 -S $SNAME -X stuff '$COMMAND'$'\n'"
- 	
- 		# Screen version 4.01.00devel (GNU) 2-May-06
-		#~ su $USER "-c screen -p 0 -S $SNAME -X stuff '$COMMAND\n'"
-
-		su $USER "-c screen -U -p 0 -S $SNAME -X stuff '$COMMAND
-		'"
-		
-		;;
-	*)
-		echo "Unknown type"
-		;;
-esac
+parse_options "$@"
+main
