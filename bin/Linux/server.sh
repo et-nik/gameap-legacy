9f7e3430a4ed21f0b71afcd1a1c77a2948a655e6
et-nik <nikita.hldm@gmail.com>
2020-04-23 22:10:49 +0300

    Fix su

--- bin/Linux/server.sh
+++ bin/Linux/server.sh
@@ -6,10 +6,10 @@
 # Autor: ET-NiK
 # Site 1: http://www.gameap.ru
 # Site 2: http://hldm.org
-# 
+#
 # Данный скрипт используется для управления игровыми серверами через АдминПанель
 #
-# 
+#
 # Параметры
 #
 # -t <type> 		(start|stop|restart|status|get_console|send_command)
@@ -22,7 +22,7 @@
 # -m <memory>		лимит оперативной памяти (Kb)
 # -f <percentage>	лимит на использование процессора
 # -n <max speed>	лимит на использование пропускной способности
-# 
+#
 # Examples:
 # ./server.sh -t start -d /home/hl_server -n screen_hldm -i 127.0.0.1 -p 27015 -c "hlds_run -game valve +ip 127.0.0.1 +port 27015 +map crossfire"
 # ./server.sh -t get_console -n hldm -u usver
@@ -44,22 +44,35 @@ server_start()
         echo -e "Server not started";
         return 1;
     fi
-    
+
     if [[ $COMMAND == "" ]]; then
         echo -e "Command empty";
         echo -e "Server not started";
         return 1;
     fi
-    
+
     if server_status; then
         echo -e "Server is already running";
         return 1
     else
-        su $UUSER -c "cd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
+        if [[ $(id -u) -eq "0" ]]; then
+            su $UUSER -c "cd $DIR; ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND";
+        else
+            cd $DIR;
+            ${COMMAND_PARTS[0]} screen -U -m -d -S $SNAME ${COMMAND_PARTS[1]} $COMMAND
+            cd -
+        fi;
+
         PID=$!;
         sleep 3;
-        
-        if su $UUSER -c "screen -U -ls | grep -i ${SNAME}"; then
+
+        if [[ $(id -u) -eq "0" ]]; then
+            check_result=$(su $UUSER -c "screen -U -ls | grep -i ${SNAME}")
+        else
+            check_result=$(screen -U -ls | grep -i ${SNAME})
+        fi;
+
+        if [[ -n ${check_result} ]]; then
             echo -e "Server started";
             return 0
         else
@@ -79,10 +92,16 @@ server_stop()
         echo -e "Server not started";
         return 1
     fi
-    
+
     if server_status; then
        kill -TERM `ps aux | grep -v grep | grep -i screen | grep -i $SNAME | awk '{print $2}'`
-       su $UUSER -c "screen -U -X -S $SNAME kill"
+
+       if [[ $(id -u) -eq "0" ]]; then
+           su $UUSER -c "screen -U -X -S $SNAME kill"
+       else
+           screen -U -X -S $SNAME kill
+       fi;
+
        echo "Server stopped"
        return 0
     else
@@ -100,7 +119,13 @@ server_status()
         return 1
     fi
 
-    if su $UUSER -c "screen -ls | grep ${SNAME}"; then
+    if [[ $(id -u) -eq "0" ]]; then
+        check_result=$(su $UUSER -c "screen -U -ls | grep -i ${SNAME}")
+    else
+        check_result=$(screen -U -ls | grep -i ${SNAME})
+    fi;
+
+    if [[ -n ${check_result} ]]; then
         return 0
     else
         return 1
@@ -114,7 +139,7 @@ get_parts()
     if [[ $RAM_LIMIT > 0 && $allow_ram_limit ]]; then
         COMMAND_PARTS[0]="ulimit -Hv $RAM_LIMIT ;";
     fi
-    
+
     if [[ $NET_LIMIT > 0 && $allow_net_limit ]]; then
         COMMAND_PARTS[1]="trickle -d $NET_LIMIT -u $NET_LIMIT";
     fi
@@ -126,10 +151,10 @@ cpu_limit()
 {
     if [[ $CPU_LIMIT && $allow_cpu_limit && $PID ]]; then
         CPU_LIMIT=$(($CPU_LIMIT*$core_count))
-        
+
         # PID
         cpulimit --pid=$PID --limit=$CPU_LIMIT
-        
+
         # EXE
         #cpulimit --exe="$DIR/$PROGRAM" --limit=$CPU_LIMIT
     fi
@@ -141,7 +166,7 @@ parse_options()
     while [[ $# -gt 0 ]]
     do
         key="$1"
-        
+
         case $key in
             -t|--type)
                 TYPE="$2"
@@ -211,21 +236,21 @@ main()
     do
         ARGUMENTS=$ARGUMENTS" $element";
     done
-    
+
     unset explode;
-    
+
     case "$TYPE" in
         start)
             get_parts;
-            
+
             if server_start; then
                 exit 0
             else
                exit 1
             fi
-            
+
             ;;
-    
+
         stop)
             if server_stop; then
                 exit 0
@@ -233,11 +258,11 @@ main()
                exit 1
             fi
             ;;
-            
+
         restart)
             get_parts >> /dev/null
             server_stop >> /dev/null
-            
+
             if server_start; then
                 echo -e "Server restarted"
                 exit 0
@@ -245,9 +270,9 @@ main()
                 echo -e "Server not restarted"
                 exit 1
             fi
-            
+
             ;;
-            
+
         status)
             if server_status; then
                echo "Server is UP"
@@ -257,26 +282,26 @@ main()
                exit 1
             fi
             ;;
-            
+
         get_console)
             screen_id=$(su gameap -c "screen -ls | grep $SNAME" | head | cut -d. -f1 | tr -d '\t')
-            
+
             su $UUSER -c "screen -U -S ${screen_id} -X -p 0 hardcopy -h ${DIR}/gap_console.txt && chmod 666 ${DIR}/gap_console.txt"
             sed -i '/^$/d' "${DIR}/gap_console.txt"
             iconv -c -f utf-8 -t utf-8 "${DIR}/gap_console.txt"
             ;;
-            
+
         send_command)
             screen_id=$(su gameap -c "screen -ls | grep $SNAME" | head | cut -d. -f1 | tr -d '\t')
             # Screen version 4.00.03jw4 (FAU) 2-May-06
             #~ su $USER "-c screen -p 0 -S $SNAME -X stuff '$COMMAND'$'\n'"
-        
+
             # Screen version 4.01.00devel (GNU) 2-May-06
             #~ su $USER "-c screen -p 0 -S $SNAME -X stuff '$COMMAND\n'"
-    
+
             su $UUSER "-c screen -U -p 0 -S ${screen_id} -X stuff '${COMMAND}
             '"
-            
+
             ;;
         *)
             echo "Unknown type"
